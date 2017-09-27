dfbb9f4acf15f26c1ebe7921ae1386434f004d52
Jeremy Bolding <cyberchefjay@gmail.com>
2017-09-27 17:23:59 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+﻿# All To Mp3
+
+[![Chocolatey Version][choco_version_svg]][choco_url]
+[![Chocolatey Downloads][choco_downloads_svg]][choco_url]
+
+> A Chocolatey port of AllToMP3
+
+
+
+## Installation
+
+```bash
+choco install all-to-mp3 -y
+```
+
+choco_downloads_svg: https://img.shields.io/chocolatey/dt/all-to-mp3.svg?color=blue?style=flat-square
+choco_version_svg: https://img.shields.io/chocolatey/v/all-to-mp3.svg?color=blue?style=flat-square
+choco_url: https://chocolatey.org/packages/all-to-mp3/ 
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,43 @@
+﻿
+$version = "0.3.2"
+
+
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = "https://www.github.com/AllToMP3/alltomp3-app/releases/download/v`"$($version)/AllToMP3-Setup-$($version).exe"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'AllToMP3'
+
+  checksum      = '022B1AC4800B67B052FD3873246B3E6F8D4D7FDE3D774183CEECE4FD89A7904B'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
